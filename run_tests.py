fda76d83e91a61237e59277f89d0ffc7e268cc1f
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2024-03-27 17:26:36 +0200

    Script design

--- run_tests.py
+++ run_tests.py
@@ -3,4 +3,31 @@ import requests
 r = requests.get("https://www.fdsn.org/ws/networks/1/query")
 
 for net in r.json()["networks"]:
-    print(net["fdsn_code"])
+    # doi test
+    if net["doi"]:
+        # success in database
+        # store doi
+    else:
+        # fail in database
+        # clear comment
+        continue
+    # datacite tests
+    dataciteapi = "https://api.datacite.org/application/vnd.datacite.datacite+json/"
+    r = requests.get(dataciteapi+net["doi"])
+    datacite = r.json()
+    # page test
+    r.request.get(datacite["url"])
+    if r.status_code == 200:
+        # success in database
+    else:
+        # fail in database
+    # open test UNSURE ABOUT THAT
+    # license test
+    if datacite['rightsList']:
+        # success in database
+        # store license
+    else:
+        # fail in database
+        # clear comment
+    # store publisher in database
+    # StationXML test UNSURE HOW TO
