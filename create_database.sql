9d1c44a53d8de6c6c6bc826141d4cc3cb4fac380
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2024-03-27 19:36:41 +0200

    Db in script

--- create_database.sql
+++ create_database.sql
@@ -5,6 +5,7 @@ CREATE TABLE networks_tests (
     test_time TIMESTAMP NOT NULL,
     name VARCHAR(500) NOT NULL,
     code VARCHAR(10) NOT NULL,
+    startdate DATE NOT NULL,
     doi_result BOOLEAN NOT NULL,
     doi_comment VARCHAR(500),
     page_result BOOLEAN NOT NULL,
--- run_tests.py
+++ run_tests.py
@@ -1,16 +1,34 @@
 import requests
+import logging
+from mysql import connector
+from datetime import datetime
 
+logging.basicConfig(level=logging.DEBUG)
+
+
+try:
+    cnx = connector.connect(user='root',
+                            password='root',
+                            host='localhost',
+                            database='netstests')
+except:
+    logging.error('Database connection problem!')
+cursor = cnx.cursor()
+
+logging.info("Getting all networks from FDSN")
 r = requests.get("https://www.fdsn.org/ws/networks/1/query")
+
+current_time = datetime.now()
 
 for net in r.json()["networks"]:
     # doi test
-    if net["doi"]:
-        # success in database
-        # store doi
-    else:
-        # fail in database
-        # clear comment
+    doi_result = True if net["doi"] else False
+    doi_comment = net["doi"] if net["doi"] else None
+    query = "INSERT INTO networks_tests (test_time, name, code, startdate, doi_result, doi_comment) VALUES (%s, %s, %s, %s, %s, %s);"
+    cursor.execute(query, (current_time, net["name"], net["fdsn_code"], net["start_date"], doi_result, doi_comment))
+    if not net["doi"]:
         continue
+
     # datacite tests
     dataciteapi = "https://api.datacite.org/application/vnd.datacite.datacite+json/"
     r = requests.get(dataciteapi+net["doi"])
@@ -19,6 +37,7 @@ for net in r.json()["networks"]:
     r.request.get(datacite["url"])
     if r.status_code == 200:
         # success in database
+        #query = "UPDATE networks_tests SET doi_result = %s WHERE test_time = %s AND name = %s;"
     else:
         # fail in database
     # open test UNSURE ABOUT THAT
@@ -30,4 +49,10 @@ for net in r.json()["networks"]:
         # fail in database
         # clear comment
     # store publisher in database
+
     # StationXML test UNSURE HOW TO
+
+    # commit changes for network net
+    cnx.commit()
+
+cursor.close()
