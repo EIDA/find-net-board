1c67d1adbe420d72a8756a3356a04161d1080656
Vasilis Petrakopoulos <vasilis4998@gmail.com>
2024-04-19 16:16:35 +0300

    Admin views

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('admin/update_db_from_sources/', views.update_db_from_sources, name='execute_custom_action'),
 ]
--- board/views.py
+++ board/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.utils import timezone
+from django.http import HttpResponse
 
-from .models import Test
+from .models import Test, Network, Datacenter
 
+import requests
+import logging
+from urllib.parse import urlparse
+
+logger = logging.getLogger(__name__)
+
+# main home page view
 def index(request):
-    latest_tests = Test.objects.order_by("-test_time")[:100]
-    context = {"latest_tests": latest_tests}
+    tests = Test.objects.order_by("-test_time")
+    context = {"tests": tests}
     return render(request, "board/index.html", context)
+
+
+# below view can only be used by admin to update database and run tests on will
+def is_admin(user):
+    return user.is_authenticated and user.is_staff
+
+@user_passes_test(is_admin)
+def update_db_from_sources(request):
+    datacenters_datasets, datacenters_urls = get_FDSN_datacenters()
+    #eida_routing = get_EIDA_routing()
+    #process_networks()
+    return HttpResponse(status=200)
+
+def get_FDSN_datacenters():
+    logger.info("Getting all datacenters from FDSN and updating database")
+    r = requests.get("https://www.fdsn.org/ws/datacenters/1/query?includedatasets=true")
+    datacenters_datasets = {}
+    datacenters_urls = {}
+    for dc in r.json()["datacenters"]:
+        station_url = None
+        for r in dc["repositories"]:
+            if r["name"] in ["archive", "ARCHIVE", "SEED"] and "datasets" in r:
+                datacenters_datasets[dc["name"]] = r["datasets"]
+                for s in r["services"]:
+                    if s["name"] == "fdsnws-station-1":
+                        station_url = s["url"]
+        datacenters_urls[dc["name"]] = urlparse(station_url).netloc if station_url is not None else ''
+        Datacenter(name=dc["name"], station_url=station_url).save()
+    return datacenters_datasets, datacenters_urls
